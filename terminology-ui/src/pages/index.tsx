02d92ae033b86dc414ef3cb23be06ebc54e89023
Joonas Kesäniemi <joonas.kesaniemi@csc.fi>
2024-01-24 14:17:10 +0200

    Update index.tsx

--- terminology-ui/src/pages/index.tsx
+++ terminology-ui/src/pages/index.tsx
@@ -26,7 +26,8 @@ interface IndexPageProps extends CommonContextState {
 
 export default function IndexPage(props: IndexPageProps) {
   const { t } = useTranslation('common');
-
+  console.log('index page');
+  console.log(props.user);
   return (
     <CommonContextProvider value={props}>
       <Layout user={props.user} fakeableUsers={props.fakeableUsers}>
@@ -35,7 +36,7 @@ export default function IndexPage(props: IndexPageProps) {
           title={t('terminology-site-title')}
           description={t('terminology-search-info')}
         />
-        <div>{props.user}</div>
+        
         <TerminologySearch />
       </Layout>
     </CommonContextProvider>
