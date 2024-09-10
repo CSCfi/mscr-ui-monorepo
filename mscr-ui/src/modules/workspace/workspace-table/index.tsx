49cad6f775a927ef0ee4aaadb258419744c670b9
rquazi <rumana.quazi@csc.fi>
2024-09-10 12:46:40 +0300

    Added format column in workspace schema table

--- mscr-ui/src/modules/workspace/workspace-table/index.tsx
+++ mscr-ui/src/modules/workspace/workspace-table/index.tsx
@@ -32,6 +32,7 @@ export default function WorkspaceTable({
     t('workspace.state'),
     t('workspace.numberOfRevisions'),
     t('workspace.pid'),
+    ...((contentType == Type.Schema) ? [t('metadata.format')] : []),
     '',
   ];
 
